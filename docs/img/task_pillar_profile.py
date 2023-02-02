b4b0e7b7ac8991eccdb31b2af9b64c8f3dc24e4f
Max Weiner <max.weiner@imf.tu-freiberg.de>
2023-02-02 13:06:03 +0100

    ext docs

--- docs/img/task_pillar_profile.py
+++ docs/img/task_pillar_profile.py
@@ -25,12 +25,21 @@ def task_pillar_profile(produces: dict[Any, Path]):
 
     for i, c in enumerate(p.pillar_sections):
         z, y = np.array(c.boundary.xy)
-        ax.fill(np.concatenate([-z, z]), np.concatenate([y, y]), alpha=0.5, label=f"pillar {i}")
+        ax.fill(np.concatenate([-z, z]), np.concatenate([y, y]), alpha=0.5)
 
     ax.plot(*p.cross_section.boundary.xy, c="k")
 
     ax.axvline(0, c="k", ls="-.")
     ax.axhline(0, c="k", ls="-.")
+
+    ax.stem(
+        p.pillars, p.pillar_heights / 2,
+        linefmt="r", markerfmt="_", basefmt="r-",
+        label="half pillar heights",
+    )
+
+    ax.fill([], [], alpha=0.5, c="gray", label="pillar sections")
+    ax.legend()
 
     fig.tight_layout()
 
