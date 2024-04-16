048ed384ea4497fa5f684422fecad7fea774ca9b
Christoph Renzing <christoph.renzing@imf.tu-freiberg.de>
2024-04-16 08:02:31 +0200

    add hooks for angles

--- pyroll/pillar_model/roll_pass/pillar_disk_element.py
+++ pyroll/pillar_model/roll_pass/pillar_disk_element.py
@@ -36,8 +36,11 @@ class PillarDiskElement(RollPass.DiskElement):
     pillar_velocities = Hook[np.ndarray]()
     """Array of velocity values for each pillar."""
 
-    pillar_angles = Hook[np.ndarray]()
-    """Array of pillars' angles towards the roll.'"""
+    pillar_longitudinal_angles = Hook[np.ndarray]()
+    """Array of pillars' angles between pillar and tooling (roll) in rolling (x) direction."""
+
+    pillar_latitudinal_angles = Hook[np.ndarray]()
+    """Array of pillars' angles between pillar and tooling (roll) in width (z) direction."""
 
 pyroll.core.root_hooks.add(pyroll.core.RollPass.DiskElement.OutProfile.pillars)
 pyroll.core.root_hooks.add(pyroll.core.RollPass.DiskElement.OutProfile.pillar_heights)
