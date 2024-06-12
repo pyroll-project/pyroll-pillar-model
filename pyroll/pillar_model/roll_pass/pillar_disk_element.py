fee577c7bdcf7e02a568cd9cc66f4a3f8aaaabbd
Christoph Renzing <64863686+ChRen95@users.noreply.github.com>
2024-06-12 15:10:13 +0200

    added pillar_strains to root hooks (#19)

--- pyroll/pillar_model/roll_pass/pillar_disk_element.py
+++ pyroll/pillar_model/roll_pass/pillar_disk_element.py
@@ -47,6 +47,7 @@ pyroll.core.root_hooks.add(pyroll.core.RollPass.DiskElement.OutProfile.pillars)
 pyroll.core.root_hooks.add(pyroll.core.RollPass.DiskElement.OutProfile.pillar_heights)
 pyroll.core.root_hooks.add(pyroll.core.RollPass.DiskElement.OutProfile.pillar_widths)
 pyroll.core.root_hooks.add(pyroll.core.RollPass.DiskElement.OutProfile.pillar_areas)
+pyroll.core.root_hooks.add(pyroll.core.RollPass.DiskElement.OutProfile.pillar_strains)
 
 pyroll.core.root_hooks.add(pyroll.core.Rotator.OutProfile.pillars)
 pyroll.core.root_hooks.add(pyroll.core.Rotator.OutProfile.pillar_heights)
