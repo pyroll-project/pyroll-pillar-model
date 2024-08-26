f227452c9215f31a370ef23a3e541b2ced280ee2
Christoph Renzing <christoph.renzing@imf.tu-freiberg.de>
2024-08-26 18:50:58 +0200

    added test for pillar boundaries and profile width

--- tests/test_pillar_model.py
+++ tests/test_pillar_model.py
@@ -155,3 +155,31 @@ def test_equidistant_pillar_widths(p: Union[PillarProfile, Profile]):
 
     assert np.isclose(p.pillar_widths[0], p.pillar_widths[-1] / 2, rtol=1e-3)
     assert np.all(np.isclose(p.pillar_widths[1:], p.pillar_widths[1], rtol=1e-3))
+
+@pytest.mark.parametrize(
+    "p", [
+        Profile.round(radius=10),
+        Profile.square(side=10, corner_radius=1),
+        Profile.box(height=10, width=5, corner_radius=1),
+        Profile.diamond(height=5, width=10, corner_radius=1)
+    ]
+)
+def test_equidistant_pillar_boundary_is_profile_boundary_(p: Union[PillarProfile, Profile]):
+    pyroll.pillar_model.Config.PILLAR_COUNT = 4
+    pyroll.pillar_model.Config.PILLAR_TYPE = "EQUIDISTANT"
+
+    assert np.isclose(p.pillar_boundaries[-1], p.width / 2, rtol=1e-3)
+
+@pytest.mark.parametrize(
+    "p", [
+        Profile.round(radius=10),
+        Profile.square(side=10, corner_radius=1),
+        Profile.box(height=10, width=5, corner_radius=1),
+        Profile.diamond(height=5, width=10, corner_radius=1)
+    ]
+)
+def test_uniform_pillar_boundary_is_profile_boundary_(p: Union[PillarProfile, Profile]):
+    pyroll.pillar_model.Config.PILLAR_COUNT = 4
+    pyroll.pillar_model.Config.PILLAR_TYPE = "UNIFORM"
+
+    assert np.isclose(p.pillar_boundaries[-1], p.width / 2, rtol=1e-3)
