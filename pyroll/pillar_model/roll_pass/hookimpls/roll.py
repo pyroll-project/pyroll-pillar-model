bd2d4aabf6a6647c1d6a9146051fd5e7d5d70c2c
Christoph Renzing <christoph.renzing@imf.tu-freiberg.de>
2025-04-16 09:17:29 +0200

    updated calculation of contact length and total pillar contact lengths

--- pyroll/pillar_model/roll_pass/hookimpls/roll.py
+++ pyroll/pillar_model/roll_pass/hookimpls/roll.py
@@ -3,17 +3,21 @@ import numpy as np
 from pyroll.core import RollPass
 
 
+@RollPass.Roll.total_pillar_contact_lengths
+def total_pillar_contact_lengths(self: RollPass.Roll):
+    contact_lengths = np.zeros_like(self.roll_pass.in_profile.pillars)
+
+    for de in self.roll_pass.disk_elements:
+        for i, pillars in enumerate(de.in_profile.pillars):
+            if de.pillars_in_contact[i]:
+                contact_lengths[i] += de.length
+
+
 @RollPass.Roll.contact_area
 def roll_contact_area(self: RollPass.Roll):
     return self.roll_pass.contact_area / 2
 
 
-@RollPass.Roll.total_pillar_contact_lengths
-def total_pillar_contact_lengths(self: RollPass.Roll):
-    rp = self.roll_pass
-    for de in rp.disk_elements:
-        for i in range(len(de.in_profile.pillars)):
-            if not de.pillars_in_contact[i]:
-                continue
-            de_pillar_contact_lengths = [de.length for de in self.disk_elements]
-            return np.sum(de_pillar_contact_lengths, axis=0)
+@RollPass.Roll.contact_length
+def contact_length(self: RollPass.Roll):
+    return np.mean(self.total_pillar_contact_lengths)
