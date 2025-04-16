4372a252c3c8556ba739fc4f171d84e05b2a773e
Christoph Renzing <christoph.renzing@imf.tu-freiberg.de>
2025-04-16 11:14:39 +0200

    added contact areas

--- pyroll/pillar_model/roll_pass/hookimpls/roll.py
+++ pyroll/pillar_model/roll_pass/hookimpls/roll.py
@@ -28,6 +28,19 @@ def roll_contact_area(self: RollPass.Roll):
     return contact_areas
 
 
+@RollPass.Roll.total_pillar_contact_areas
+def roll_contact_area(self: RollPass.Roll):
+    contact_areas = np.zeros_like(self.roll_pass.in_profile.pillars)
+
+    for de in self.roll_pass.disk_elements:
+        for i, pillars in enumerate(de.in_profile.pillars):
+            if de.pillars_in_contact[i]:
+                area = (de.in_profile.pillar_widths[i] + de.out_profile.pillar_widths[i]) / 2 * de.length
+                contact_areas[i] += area
+
+    return contact_areas
+
+
 @RollPass.Roll.contact_area
 def roll_contact_area(self: RollPass.Roll):
     return self.roll_pass.contact_area / 2
